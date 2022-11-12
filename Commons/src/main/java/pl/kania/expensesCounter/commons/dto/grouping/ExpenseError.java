d45077fa41581401052dabc1549e94974a6bbbc9
jaskier07 <aleksanderkania96@gmail.com>
2022-11-12 23:09:41 +0100

    Made ExpenseError compliant with Jackson

--- Commons/src/main/java/pl/kania/expensesCounter/commons/dto/grouping/ExpenseError.java
+++ Commons/src/main/java/pl/kania/expensesCounter/commons/dto/grouping/ExpenseError.java
@@ -1,9 +1,13 @@
 package pl.kania.expensesCounter.commons.dto.grouping;
 
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.Value;
 import pl.kania.expensesCounter.commons.dto.extraction.ParsedExpense;
 
 @Value
+@NoArgsConstructor(force = true)
+@RequiredArgsConstructor
 public class ExpenseError {
     ParsedExpense expense;
     String errorMessage;
